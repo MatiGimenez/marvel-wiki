b28e59ae3dcc5b1727397c8b617d63d6f192867e
Matias Gimenez <meganiunmati@hotmail.com>
2021-03-08 03:00:38 -0300

    consolelogs

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,8 @@ const DATE = date.getTime();
 
 const API_KEY = "801ba33176620102b5413c542a89c925";
 const PRIVATE_KEY = process.env.SECRET_KEY;
+console.log(process.env);
+console.log(PRIVATE_KEY);
 const HASH = crypto
   .createHash("md5")
   .update(DATE.toString())
