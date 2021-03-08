c1118a5c9f839a378bac5e885170287ef77d2be8
Matias Gimenez <meganiunmati@hotmail.com>
2021-03-08 03:42:44 -0300

    div added

--- src/components/molecules/CharacterCard.jsx
+++ src/components/molecules/CharacterCard.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const CharacterCard = ({name, thumbnail}) => {
 
   return (
-    <>
+    <div>
       <div>
         <img width={200} height={300} alt="" src={`${thumbnail.path}.${thumbnail.extension}`} />
       </div>
       <h3>{name}</h3>
-    </>
+    </div>
   )
 }
 
