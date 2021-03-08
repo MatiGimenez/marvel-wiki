0593df2858285f82c18a65781aceefb13abcd766
Matias Gimenez <meganiunmati@hotmail.com>
2021-03-08 01:45:05 -0300

    Revert "first commit"
    This reverts commit 8b89cbdc1561fcc5a0f52935aa07d3e953812a60.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route } from "react-router-dom";
-import Main from "./components/pages/Main";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Route path="/" exact component={Main}></Route>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
