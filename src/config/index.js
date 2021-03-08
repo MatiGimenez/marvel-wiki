679e2f7cfaa5f81d16005a5c903a373f2bd64097
Matias Gimenez <meganiunmati@hotmail.com>
2021-03-08 02:16:52 -0300

    date toString

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ const API_KEY = "801ba33176620102b5413c542a89c925";
 const PRIVATE_KEY = process.env.SECRET_KEY;
 const HASH = crypto
   .createHash("md5")
-  .update(DATE)
+  .update(DATE.toString())
   .update(PRIVATE_KEY)
   .update(API_KEY)
   .digest("hex");
