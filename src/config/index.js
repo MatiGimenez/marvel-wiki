6d3fd9fdc600a3b85880a6dcdc3038ed001e145f
Matias Gimenez <meganiunmati@hotmail.com>
2021-03-08 03:15:20 -0300

    private key hardcoded

--- src/config/index.js
+++ src/config/index.js
@@ -4,9 +4,7 @@ let date = new Date();
 const DATE = date.getTime();
 
 const API_KEY = "801ba33176620102b5413c542a89c925";
-const PRIVATE_KEY = process.env.REACT_APP_SECRET_KEY;
-console.log(process.env);
-console.log(PRIVATE_KEY);
+const PRIVATE_KEY = "9dfaddc316d2680fad4266d79cee2fb81ef25d29";
 const HASH = crypto
   .createHash("md5")
   .update(DATE.toString())
