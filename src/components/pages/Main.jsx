dd0ae4015a727cfeeff106757cd4a9bbabe28b5f
Matias Gimenez <meganiunmati@hotmail.com>
2021-03-11 02:10:17 -0300

    added dashboard component

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components';
-import axios from 'axios';
-import { API_KEY, HASH, DATE } from '../../config'
-import CharacterCard from '../molecules/CharacterCard';
-
-const StyledMain = styled.div`
-  display: grid;
-  grid-template: '1fr 1fr 1fr';
-  row-gap: 10px;
-`;
+import React from 'react'
+import Dashboard from '../templates/Dashboard'
+import './Main.css'
 
 const Main = () => {
 
-  const [characters, setCharacters] = useState([])
-  
-  useEffect(() => {
-    let mounted = true;
-    
-    axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=${DATE}&apikey=${API_KEY}&hash=${HASH}`)
-      .then(response => {
-        if (mounted) {
-          console.log(response.data.data.results)
-          setCharacters(response.data.data.results)
-        }});
-
-    return () => mounted = false;
-  }, [])
-
   return (
-    <StyledMain>
-      {characters && characters.map(character => <CharacterCard key={character.id} name={character.name} thumbnail={character.thumbnail} />)}
-    </StyledMain>
+    <div className="main-page">
+      <Dashboard/>
+    </div>
   )
 }
 
---
+++ src/components/templates/Dashboard.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { API_KEY, HASH, DATE } from '../../config'
+import CharacterCard from '../molecules/CharacterCard'
+import './Dashboard.css'
+
+const Dashboard = () => {
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    let mounted = true;
+    
+    axios.get(`https://gateway.marvel.com:443/v1/public/characters?ts=${DATE}&apikey=${API_KEY}&hash=${HASH}`)
+      .then(response => {
+        if (mounted) {
+          console.log(response.data.data.results)
+          setCharacters(response.data.data.results)
+        }});
+
+    return () => mounted = false;
+  }, [])
+
+  return (
+    <div className="dashboard">
+      {characters && characters.map(character => <CharacterCard key={character.id} name={character.name} thumbnail={character.thumbnail} />)}
+    </div>
+  )
+}
+
+export default Dashboard;
