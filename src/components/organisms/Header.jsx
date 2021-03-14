f675024c5d72ef004e0d0f96b1a5f7f44beb2693
Matias Gimenez <meganiunmati@hotmail.com>
2021-03-14 13:08:49 -0300

    testing dinamic header

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Header.scss'
 
 const Header = () => {
 
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 700;
+
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    // subscribe to window resize event "onComponentDidMount"
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      // unsubscribe "onComponentDestroy"
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
   return (
     <div className="header">
-      <div>Boton 1</div>
-      <div>Boton 2</div>
-      <div>Boton 3</div>
-      <div>Boton 4</div>
+      {
+        width < breakpoint ?
+          <i class="fas fa-bars"></i> 
+          : <>
+              <div>Boton 1</div>
+              <div>Boton 2</div>
+              <div>Boton 3</div>
+              <div>Boton 4</div>
+            </>
+      }
     </div>
   )
 }
