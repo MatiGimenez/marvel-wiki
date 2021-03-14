a5cc3284a9ca12b15c09b3b0525794ce49f1ff93
Matias Gimenez <meganiunmati@hotmail.com>
2021-03-14 20:46:03 -0300

    added nav to paginator component + test scrollbar

--- src/components/molecules/Paginator.jsx
+++ src/components/molecules/Paginator.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Paginator = () => {
-
   return (
-    <div>THIS IS MY PAGINATOR</div>
-  )
-}
+    <div className="paginator-container">
+      <nav className="nav-container">
+        <ul className="page-buttons">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Paginator;
